75eef13d746eff19067926c88bc36a5aa761e4b7
Cristofep <srcristofep@gmail.com>
2024-03-13 14:32:24 -0400

    Bug fixes in run.py file
    Signed-off-by: Cristofep <srcristofep@gmail.com>

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask('__name__')
 
 # Home
@@ -18,8 +19,8 @@ def user(name = None, edad = None):
 # Calculadora
 
 # suma
-@app.route('/suma/<int:num1>,<int:num2>')
-@app.route('/suma/<int:num1>,<int:num2>,<int:num3>')
+@app.route('/suma/<int:num1>/<int:num2>')
+@app.route('/suma/<int:num1>/<int:num2>/<int:num3>')
 def suma(num1, num2, num3=None):
     if num3 is None:
         resultado = num1 + num2
@@ -29,8 +30,8 @@ def suma(num1, num2, num3=None):
         return f'<center><h1>{num1} + {num2} + {num3} = {resultado}</h1></center>'
 
 # resta
-@app.route('/resta/<int:num1>,<int:num2>')
-@app.route('/resta/<int:num1>,<int:num2>,<int:num3>')
+@app.route('/resta/<int:num1>/<int:num2>')
+@app.route('/resta/<int:num1>/<int:num2/,<int:num3>')
 def resta(num1, num2, num3=None):
     if num3 is None:
         resultado = num1 - num2
@@ -40,8 +41,8 @@ def resta(num1, num2, num3=None):
         return f'<center><h1>{num1} - {num2} - {num3} = {resultado}</h1></center>'
 
 # multiplicacion
-@app.route('/multiplicacion/<int:num1>,<int:num2>')
-@app.route('/multiplicacion/<int:num1>,<int:num2>,<int:num3>')
+@app.route('/multiplicacion/<int:num1>/<int:num2>')
+@app.route('/multiplicacion/<int:num1>/<int:num2>/<int:num3>')
 def multiplicacion(num1, num2, num3=None):
     if num3 is None:
         resultado = num1 * num2
@@ -51,8 +52,8 @@ def multiplicacion(num1, num2, num3=None):
         return f'<center><h1>{num1} x {num2} x {num3} = {resultado}</h1></center>'
 
 # division
-@app.route('/division/<int:num1>,<int:num2>')
-@app.route('/division/<int:num1>,<int:num2>,<int:num3>')
+@app.route('/division/<int:num1>/<int:num2>')
+@app.route('/division/<int:num1>/<int:num2>/<int:num3>')
 def division(num1, num2, num3=None):
     if num3 is None:
         resultado = num1 / num2
@@ -62,7 +63,7 @@ def division(num1, num2, num3=None):
         return f'<center><h1>{num1} ÷ {num2} ÷ {num3} = {resultado}</h1></center>'
 
 # potencia
-@app.route('/potencia/<int:num1>,<int:num2>')
+@app.route('/potencia/<int:num1>/<int:num2>')
 def potencia(num1,num2):
     resultado = num1 ** num2
     return f'<center><h1>{num1} ^ {num2} = {resultado}</h1></center>'
