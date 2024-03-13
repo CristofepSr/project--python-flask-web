50aeac0f70cf627419c15de37c7729159d9e794b
Cristofep <srcristofep@gmail.com>
2024-03-13 13:28:21 -0400

    File python run.py

---
+++ run.py
@@ -0,0 +1,62 @@
+from flask import Flask
+
+app = Flask('__name__')
+
+# Home
+@app.route('/')
+def index():
+    return '<center><h1>Bienbenido</h1></center>'
+
+# Nombre y Edad
+@app.route('/<name>')
+@app.route('/<name>/<edad>')
+def user(name = None, edad = None):
+    if edad == None:
+        return f'<center><h1>Tu nombre es: {name}</h1></center>'
+    else:
+        return f'<center><h1>Tu nombre es: {name}, y tu edad: {edad}</h1></center>'
+
+# Calculadora
+@app.route('/suma/<int:num1>,<int:num2>')
+@app.route('/suma/<int:num1>,<int:num2>,<int:num3>')
+def suma(num1, num2, num3=None):
+    if num3 is None:
+        resultado = num1 + num2
+        return f'<center><h1>{num1} + {num2} = {resultado}</h1></center>'
+    else:
+        resultado = num1 + num2 + num3
+        return f'<center><h1>{num1} + {num2} + {num3} = {resultado}</h1></center>'
+
+@app.route('/resta/<int:num1>,<int:num2>')
+@app.route('/resta/<int:num1>,<int:num2>,<int:num3>')
+def resta(num1, num2, num3=None):
+    if num3 is None:
+        resultado = num1 - num2
+        return f'<center><h1>{num1} - {num2} = {resultado}</h1></center>'
+    else:
+        resultado = num1 - num2 - num3
+        return f'<center><h1>{num1} - {num2} - {num3} = {resultado}</h1></center>'
+    
+@app.route('/multiplicacion/<int:num1>,<int:num2>')
+@app.route('/multiplicacion/<int:num1>,<int:num2>,<int:num3>')
+def multiplicacion(num1, num2, num3=None):
+    if num3 is None:
+        resultado = num1 * num2
+        return f'<center><h1>{num1} * {num2} = {resultado}</h1></center>'
+    else:
+        resultado = num1 * num2 * num3
+        return f'<center><h1>{num1} * {num2} * {num3} = {resultado}</h1></center>'
+
+@app.route('/division/<int:num1>,<int:num2>')
+@app.route('/division/<int:num1>,<int:num2>,<int:num3>')
+def division(num1, num2, num3=None):
+    if num3 is None:
+        resultado = num1 / num2
+        return f'<center><h1>{num1} / {num2} = {resultado}</h1></center>'
+    else:
+        resultado = num1 / num2 / num3
+        return f'<center><h1>{num1} / {num2} / {num3} = {resultado}</h1></center>'
+
+
+if __name__ == "__main__":
+    app.run()
