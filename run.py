9a35e12b231d4f338b64f1151f9d9dd1ec449f5f
Cristofep <srcristofep@gmail.com>
2024-03-15 09:35:53 -0400

    Improving mathematical operations
    Signed-off-by: Cristofep <srcristofep@gmail.com>

--- run.py
+++ run.py
@@ -17,56 +17,42 @@ def user(name = None, edad = None):
         return f'<center><h1>Tu nombre es: {name}, y tu edad: {edad}</h1></center>'
 
 # Calculadora
+@app.route('/calculadora')
+def calculadora():
+    return '<center><h1>Calculadora</h1></center>'
 
-# suma
-@app.route('/suma/<int:num1>/<int:num2>')
-@app.route('/suma/<int:num1>/<int:num2>/<int:num3>')
-def suma(num1, num2, num3=None):
-    if num3 is None:
-        resultado = num1 + num2
-        return f'<center><h1>{num1} + {num2} = {resultado}</h1></center>'
+# Calculadora de 2 numeros
+@app.route('/calculadora/<Signo>/<int:num1>/<int:num2>')
+def calculadora1(signo, num1, num2):
+    if signo == 'suma':
+        result = num1 + num2
+        return f'<center><h1>{num1} + {num2} = {num1 + num2}</h1></center>'
+    elif signo == 'resta':
+        result = num1 - num2
+        return f'<center><h1>{num1} - {num2} = {num1 - num2}</h1></center>'
+    elif signo == 'multiplicacion':
+        result = num1 * num2
+        return f'<center><h1>{num1} x {num2} = {num1 * num2}</h1></center>'
+    elif signo == 'division':
+        result = num1 / num2
+        return f'<center><h1>{num1} ÷ {num2} = {num1 / num2}</h1></center>'
+    elif signo == "potencia":
+        result = num1 ** num2
+        return f'<center><h1>{num1} ^ {num2} = {result}</h1></center>'
     else:
-        resultado = num1 + num2 + num3
-        return f'<center><h1>{num1} + {num2} + {num3} = {resultado}</h1></center>'
+        return f'<center><h1>ERROR</h1></center>'
 
-# resta
-@app.route('/resta/<int:num1>/<int:num2>')
-@app.route('/resta/<int:num1>/<int:num2/,<int:num3>')
-def resta(num1, num2, num3=None):
-    if num3 is None:
-        resultado = num1 - num2
-        return f'<center><h1>{num1} - {num2} = {resultado}</h1></center>'
+# Calculadora de 3 numeros
+@app.route('/calculadora/<signo>/<int:num1>/<int:num2>/<int:num3>')
+def calculadora2(signo, num1, num2, num3 = None):
+    if signo == 'suma':
+        result = num1 + num2 + num3
+        return f'<center><h1>{num1} + {num2} + {num3} = {result}</h1></center>'
+    elif signo == 'resta':
+        result = num1 - num2 - num3
+        return f'<center><h1>{num1} - {num2} - {num3} = {result}</h1></center>'
     else:
-        resultado = num1 - num2 - num3
-        return f'<center><h1>{num1} - {num2} - {num3} = {resultado}</h1></center>'
-
-# multiplicacion
-@app.route('/multiplicacion/<int:num1>/<int:num2>')
-@app.route('/multiplicacion/<int:num1>/<int:num2>/<int:num3>')
-def multiplicacion(num1, num2, num3=None):
-    if num3 is None:
-        resultado = num1 * num2
-        return f'<center><h1>{num1} x {num2} = {resultado}</h1></center>'
-    else:
-        resultado = num1 * num2 * num3
-        return f'<center><h1>{num1} x {num2} x {num3} = {resultado}</h1></center>'
-
-# division
-@app.route('/division/<int:num1>/<int:num2>')
-@app.route('/division/<int:num1>/<int:num2>/<int:num3>')
-def division(num1, num2, num3=None):
-    if num3 is None:
-        resultado = num1 / num2
-        return f'<center><h1>{num1} ÷ {num2} = {resultado}</h1></center>'
-    else:
-        resultado = num1 / num2 / num3
-        return f'<center><h1>{num1} ÷ {num2} ÷ {num3} = {resultado}</h1></center>'
-
-# potencia
-@app.route('/potencia/<int:num1>/<int:num2>')
-def potencia(num1,num2):
-    resultado = num1 ** num2
-    return f'<center><h1>{num1} ^ {num2} = {resultado}</h1></center>'
+        return f'<center><h1>ERROR</h1></center>'
 
 if __name__ == "__main__":
     app.run()
